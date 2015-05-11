f5f4a071e942e2660654a28141538b84f8846b9e
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-05-11 11:46:20 -0400

    Busca por país adicionada

--- Mapa/FirstViewController.h
+++ Mapa/FirstViewController.h
@@ -13,7 +13,8 @@
 
 @property (weak, nonatomic) IBOutlet MKMapView *map;
 @property (strong, nonatomic) CLLocationManager *lm;
-
+@property (weak, nonatomic) IBOutlet UISearchBar *mysearch;
+@property (nonatomic)NSInteger local;
 
 @end
 
--- Mapa/FirstViewController.m
+++ Mapa/FirstViewController.m
@@ -19,9 +19,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.mysearch.delegate = self;
+    self.map.delegate = self;
     self.lm = [[CLLocationManager alloc] init];
     [self.lm requestAlwaysAuthorization];
     
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    [self.mysearch resignFirstResponder];
+    
+    CLGeocoder *gc = [[CLGeocoder alloc] init];
+    [gc geocodeAddressString:self.mysearch.text completionHandler:^(NSArray *placemarks, NSError *error)
+    {
+        CLPlacemark *placemark = [placemarks objectAtIndex:0];
+        
+        MKCoordinateRegion region;
+        CLLocationCoordinate2D newLocation = [placemark.location coordinate];
+        region.center = [(CLCircularRegion *) placemark.region center];
+        
+        MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+        [annotation setCoordinate:newLocation];
+        [annotation setTitle:self.mysearch.text];
+        [self.map addAnnotation:annotation];
+        
+        MKMapRect mr = [self.map visibleMapRect];
+        MKMapPoint pt = MKMapPointForCoordinate([annotation coordinate]);
+        mr.origin.x = pt.x - mr.size.width * 0.5;
+        mr.origin.y = pt.y - mr.size.height * 0.25;
+        [self.map setVisibleMapRect:mr animated:YES];
+        
+        
+    }];
+    
+}
+- (IBAction)local:(id)sender {
+    _local = 1;
 }
 
 - (void)didReceiveMemoryWarning
@@ -32,8 +66,13 @@
 
 - (void) mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
 {
+    if (_local == 1) {
+        
+    
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 200, 200);
     [self.map setRegion:[self.map regionThatFits:region] animated:YES];
+        _local = 0;
+    }
 }
 
 @end
